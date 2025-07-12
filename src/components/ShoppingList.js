45fdf841d86b3631dfe85f2ad8a5a09abfdaf308
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-07-12 11:43:44 +0300

    controlled states

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,21 +5,24 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchText, setSearchText] = useState("")
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
+  function handleSearchChange(event){
+    setSearchText(event.target.value);
+  }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
-
-    return item.category === selectedCategory;
+  const itemsToDisplay = items.filter((item)=>{
+    const matchingCategory=selectedCategory==="All" || item.category===selectedCategory;
+    const matchingSearch=item.name.toLowerCase().includes(searchText.toLowerCase());
+    return matchingCategory && matchingSearch;
   });
-
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleSearchChange} searchText={searchText}/>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
